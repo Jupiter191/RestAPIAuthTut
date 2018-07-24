ff8de76689866972da1bd73b37da74b85789f2fc
Jupiter191 <liam.h.-@hotmail.com>
2018-07-25 00:36:24 +0100

    Added dependencies and required middleware

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -1,3 +1,10 @@
 'use strict';
 
 var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended : true }));
+
+var User = require('../user/User');
