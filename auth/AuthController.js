dbb20fd56a6bf1765206ac60b393ca4e7d2838f8
Jupiter191 <liam.h.-@hotmail.com>
2018-07-25 00:43:25 +0100

    Added dependencies on jsonwebtoken and bcryptjs modules to AuthController.js and saved them to package.json

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -1,8 +1,17 @@
 'use strict';
 
+// 2. Add required modules for the authentication routes
+//    and middleware.
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
+
+// 2.1 Add dependencies on JSONWebToken (NodeJS implementation
+//     of JWT), bCryptJS (a password hashing module), and the
+//     config.js file from earlier
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
+var config = require('../config');
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended : true }));
