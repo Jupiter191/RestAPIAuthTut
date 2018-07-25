b6cd2e14439b9463dea3b23207459071e994f1da
Liam Hilliard <liam.hilliard@accenture.com>
2018-07-25 09:30:51 +0100

    Added httpStatus object with enums for common HTTP status codes (200s, 400s, 500s). Added this to AuthController.js

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -6,9 +6,13 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 
+// This is a custom file containing enums of useful HTTP status codes
+// for easier readability. It it not part of the tutorial.
+var httpStatus = require('../httpStatus');
+
 // 2.1 Add dependencies on JSONWebToken (NodeJS implementation
 //     of JWT), bCryptJS (a password hashing module), and the
-//     config.js file from earlier
+//     config.js file from earlier.
 var jwt = require('jsonwebtoken');
 var bcrypt = require('bcryptjs');
 var config = require('../config');
@@ -21,14 +25,17 @@ var User = require('../user/User');
 
 
 // Register route
+// 3. This is the route used to register a new user.
 router.post('/register', function(request, response){
 
+    // This try/catch is my own addition. It is not part of the tutorial.
     try{
+        // 3.1 hashSync takes the password to hash, plus a salt length.
         var hashedPassword = bcrypt.hashSync(request.body.password, 8);
     } catch(IllegalArgumentsException){
-        
+        response.status().send();
     }
-    response.send(hashedPassword);
+    
 
 });
 
